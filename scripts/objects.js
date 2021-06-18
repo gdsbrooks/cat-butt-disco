f084fa819586a06944ae456843df3552b25f9f0d
George DS Brooks <george@gdsbrooks.com>
2021-06-18 10:09:16 +0100

    working. 3 score for demo

--- scripts/objects.js
+++ scripts/objects.js
@@ -51,7 +51,7 @@ class Obstacle {
 
     recordCounter++;
 
-    recordCounter % 12git === 0 ? (this.isRecord = true) : null;
+    recordCounter % 12 === 0 ? (this.isRecord = true) : null;
   }
   checkCollision() {
     if (
